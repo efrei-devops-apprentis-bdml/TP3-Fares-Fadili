dfec409ec82d8a2eae02b98294f4a2f80e657b44
Fares0211 <91180237+Fares0211@users.noreply.github.com>
2022-06-14 21:14:19 +0200

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,8 @@ COPY TP1.py ./
 
 # install dependencies
 RUN pip3 install --no-cache-dir -Iv requests===2.27.1
+RUN pip3 install --no-cache-dir -Iv Flask===2.1.2
 
 # command to run on container start
 CMD [ "python3", "./TP1.py" ]
+
--- TP1.py
+++ TP1.py
@@ -2,9 +2,14 @@ import requests
 import json
 import os
 from pprint import pprint
+from flask import Flask
+
+app = Flask(__name__)
 
 
+@app.route('/<lat>/<lon>')
 def weather(lat, lon, api_key):
+    cle_api = os.environ['API_KEY']
     url = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&units=metric" % (
         lat, lon, api_key)
 
@@ -13,12 +18,14 @@ def weather(lat, lon, api_key):
     json_str = json.dumps(weather_data)
     resp = json.loads(json_str)
 
-    pprint(resp['current'])
+    # pprint(resp['current'])
+
+    return resp['current']
 
 
-latitude = os.environ['LAT']
-longitude = os.environ['LONG']
-cle_api = os.environ['API_KEY']
+#latitude = os.environ['LAT']
+#longitude = os.environ['LONG']
 
-
-weather(latitude, longitude, cle_api)
+#weather(latitude, longitude, cle_api)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
